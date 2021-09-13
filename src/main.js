44c4c3c9a1ec332e94b38ce72b84ef19072dee4f
吴庆 <wuqing19980121@outlook.com>
2021-09-13 19:30:37 +0800

    进度条

--- src/main.js
+++ src/main.js
@@ -10,10 +10,21 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 在request拦截器中展示进度条
 axios.interceptors.request.use(config => {
+  Nprogress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+// 在response拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+  Nprogress.done()
   return config
 })
 Vue.prototype.$http = axios
